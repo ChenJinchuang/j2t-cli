159fce0e72df393ded90382c73c40b89f1556fa6
沁塵 <qinchenju@gmail.com>
2022-07-20 17:12:36 +0800

    feat: 非基本类型的数据提取出单独的类型声明
    feat:支持配置多个 JSON 数据的类型声明输出到同一个 d.ts文件

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import { Schedule } from "./index";
 
 export type ScheduleNode = {
     nodeName: string,
@@ -19,28 +18,30 @@ export type ASTTree = {
 
 export class Parser {
 
-    public static parse(schedule: Schedule): ASTTree[] {
-        const astTrees: ASTTree[] = []
-        for (const exportFileName in schedule) {
-            const tree: ASTTree = {
-                exportFileName: exportFileName,
-                scheduleNodes: Parser.extractScheduleNode(schedule[exportFileName]),
-            }
-            astTrees.push(tree)
-        }
-        return astTrees
+    private readonly astTree: ASTTree
+    private readonly exportFileName: string
+    private readonly scheduleData: { [symbol: string]: any }[]
+
+    constructor(exportFileName: string, scheduleData: { [symbol: string]: any }[]) {
+        this.exportFileName = exportFileName
+        this.scheduleData = scheduleData
+        this.astTree = Parser.createASTTree(this.exportFileName, [])
     }
 
-    private static extractScheduleNode(schedule: { [symbol: string]: { [symbol: string]: any } }[]): ScheduleRootNode[] {
+    public parse(): ASTTree {
+        const rootNodes = this.extractScheduleRootNode(this.scheduleData);
+        this.astTree.scheduleNodes = [...this.astTree.scheduleNodes, ...rootNodes]
+        return this.astTree
+    }
+
+    private extractScheduleRootNode(schedule: { [symbol: string]: { [symbol: string]: any } }[]): ScheduleRootNode[] {
         const rootNodes: ScheduleRootNode[] = []
         for (const scheduleElement of schedule) {
             for (const exportTypeName in scheduleElement) {
-                const nodes = Parser.extractNode(scheduleElement[exportTypeName])
-                const node: ScheduleRootNode = {
-                    exportTypeName: exportTypeName,
-                    rootNodeType: _.isArray(scheduleElement[exportTypeName]) ? "array" : "object",
-                    nodes
-                }
+                const nodes = this.extractNode(scheduleElement[exportTypeName])
+                const rootNodeType = _.isArray(scheduleElement[exportTypeName]) ? "array" : "object"
+
+                const node = Parser.createScheduleRootNode(exportTypeName, rootNodeType, nodes)
                 rootNodes.push(node)
             }
         }
@@ -48,44 +49,57 @@ export class Parser {
         return rootNodes
     }
 
-    private static extractNode(data: any) {
+    private extractNode(data: any) {
         const nodes: ScheduleNode[] = []
         if (_.isArray(data)) {
             const node = {
                 nodeName: "",
                 nodeType: "array",
-                child: Parser.extractNode(data[0])
+                child: this.extractNode(data[0])
             }
             nodes.push(node)
             return nodes;
         }
-
         for (const key in data) {
-            const isArray = _.isArray(data[key]);
             let node: ScheduleNode
-            if (isArray) {
-                node = {
-                    nodeName: key,
-                    nodeType: "array",
-                    child: typeof data[key][0] === "object"
-                        ? Parser.extractNode(data[key][0])
-                        : [{ nodeName: "", nodeType: "string", child: [] }]
-                }
+            const isArray = _.isArray(data[key]);
+
+            if (typeof data[key] === 'object' && data[key]) {
+                const rootNode = Parser.createScheduleRootNode(_.upperFirst(key),
+                    isArray ? 'array' : 'object',
+                    this.extractNode(data[key]))
+                this.astTree.scheduleNodes = [rootNode, ...this.astTree.scheduleNodes]
+                node = Parser.createScheduleNode(key, _.upperFirst(key), [])
             } else {
-                let nodeType = ""
-                nodeType = data[key] === null ? "null" : typeof data[key]
-                node = {
-                    nodeName: key,
-                    nodeType: nodeType === "object" ? isArray ? "array" : "object" : nodeType,
-                    child: []
-                }
-                node.child = typeof data[key] === "object" ? Parser.extractNode(data[key]) : []
+                let nodeType = data[key] === null ? "null" : typeof data[key]
+                node = Parser.createScheduleNode(key, nodeType, [])
             }
+
             nodes.push(node)
-
         }
-
         return nodes;
     }
 
+    private static createASTTree(exportFileName: string, rootNodes: ScheduleRootNode[]): ASTTree {
+        return {
+            exportFileName,
+            scheduleNodes: rootNodes,
+        }
+    }
+
+    private static createScheduleRootNode(exportTypeName: string, rootNodeType: string, nodes: ScheduleNode[]): ScheduleRootNode {
+        return {
+            exportTypeName,
+            rootNodeType,
+            nodes
+        }
+    }
+
+    private static createScheduleNode(nodeName: string, nodeType: string, child: ScheduleNode[]): ScheduleNode {
+        return {
+            nodeName,
+            nodeType,
+            child,
+        }
+    }
 }
